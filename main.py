5070296f446ea0d936ccd7c59eca0b207901cf00
Seokhyeon Kim <tjrgusdlrk101@gmail.com>
2024-02-05 10:20:31 +0900

    member route 작성

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 
+from app.routes.member import member_router
+
 app = FastAPI()
 
+# 외부 route 파일 불러오기
+app.include_router(member_router)
 
 @app.get("/")
 async def root():
@@ -12,3 +16,7 @@ async def root():
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
+
+if __name__ == '_main_':
+    import uvicorn
+    uvicorn.run('main:app',reload=True)
