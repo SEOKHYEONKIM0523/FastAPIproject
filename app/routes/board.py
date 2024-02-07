4d1aa5b0d2e00d9345c26d6f2d4f35d624206932
Seokhyeon Kim <tjrgusdlrk101@gmail.com>
2024-02-07 10:14:10 +0900

    게시판 - 프론트엔드

--- app/routes/board.py
+++ app/routes/board.py
@@ -1,20 +1,26 @@
 from fastapi import APIRouter
-
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.requests import Request
+from fastapi.responses import HTMLResponse
 
 board_router = APIRouter()
 
-@board_router.get('/list')
-def join():
-    return {'msg':'Hello, Board list!'}
+templates = Jinja2Templates(directory='views/templates')
+board_router.mount('/static',StaticFiles(directory='views/static'),name='static')
 
 
-@board_router.get('/write')
-def login():
-    return {'msg':'Hello, Board write!'}
+@board_router.get('/list', response_class=HTMLResponse)
+def list(req: Request):
+    return templates.TemplateResponse('board/list.html', {'request': req})
 
 
-@board_router.get('/view')
-def myinfo():
-    return {'msg':'Hello, Board view!'}
+@board_router.get('/write', response_class=HTMLResponse)
+def write(req: Request):
+    return templates.TemplateResponse('board/write.html', {'request': req})
 
+
+@board_router.get('/view', response_class=HTMLResponse)
+def view(req: Request):
+    return templates.TemplateResponse('board/view.html', {'request': req})
 
