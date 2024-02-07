250277415747223535a3cc9ac7d08e4089e7a60f
Seokhyeon Kim <tjrgusdlrk101@gmail.com>
2024-02-07 12:13:42 +0900

    게시판 리스트 - 백엔드

--- app/dbfactory.py
+++ app/dbfactory.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.settings import config
-from app.models import member, board
+from app.models import member
+from app.models import board
 
 
 # sqlite 사용시 check_same_thread 를 추가 - 스레드 사용 안함
