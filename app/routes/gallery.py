717daca3ba9b7853cdccdfeb260bc003775cd0eb
Seokhyeon Kim <tjrgusdlrk101@gmail.com>
2024-02-15 10:15:59 +0900

    갤러리 3 - 백엔드 - 파일업로드 및 gallery 테이블 저장

--- app/routes/gallery.py
+++ app/routes/gallery.py
@@ -1,5 +1,3 @@
-from math import ceil
-
 from fastapi import APIRouter, Form, UploadFile, File
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -7,8 +5,8 @@ from fastapi.requests import Request
 from fastapi.responses import HTMLResponse
 from fastapi.responses import RedirectResponse
 from fastapi import status
-from app.schemas.board import NewBoard
-from app.services.board import BoardService
+from app.schemas.gallery import NewGallery
+from app.services.gallery import GalleryService
 
 gallery_router = APIRouter()
 
@@ -40,27 +38,13 @@ def write(req: Request):
 
 @gallery_router.post('/write')
 async def writeok(title: str = Form(),userid: str = Form(),contents: str = Form(), attach: UploadFile = File() ):
-
     res_url = '/gallery/list/1'
-
-
-
     # print(title, userid, contents)
     # print(attach.filename, attach.content_type, attach.size)
 
-    UPLOAD_DIR = r'C:\Java\nginx-1.25.3\html\cdn'
-    fname = UPLOAD_DIR + r'\\20240214' + attach.filename
-
-    # 비동기 처리를 위해 함수에 await 지시자 추가
-    # 이럴 경우 함수 정의 부분에 async라는 지시자 추가 필요
-    content = await attach.read() #업로드한 파일의 내용을 비동기로 모두 읽어옴
-    with open(fname, 'wb') as f:
-        f.write(content)
-
-    # if BoardService.check_captcha(bdto): #captcha 체크가 true라면
-    #     result = BoardService.insert_gallery(bdto)
-    #     res_url = '/error'
-    #     if result.rowcount > 0: res_url = '/gallery/list/1'
+    fname, fsize = await GalleryService.process_upload(attach)
+    gdto = NewGallery(title=title, userid=userid, contents=contents)
+    GalleryService.insert_gallery(gdto, fname, fsize)
 
     return RedirectResponse(res_url, status_code=status.HTTP_302_FOUND)
 
